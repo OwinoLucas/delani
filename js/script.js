6d638f9306efb183d4cf3e1edd918cfc4eee9dbd
Lucas Owino <lucowish35@gmail.com>
2020-02-22 14:57:50 +0300

    Add JQuery fuctions to show the icon and hide the text when clicked

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function(){
         $(".img").hide();
         $(".what-showing2").show();
     });
+    $(".what-showing2").click(function(){
+        $(".what-showing2").hide();
+        $(".img").show();
+    });
+      
+    $(".DM").click(function(){
+        $(".DM").hide();
+        $(".what-showing3").show();
+    });
+    $(".what-showing3").click(function(){
+        $(".what-showing3").hide();
+        $(".DM").show();
+    });
+
+    $(".PM").click(function(){
+        $(".PM").hide();
+        $(".what-showing4").show();
+    });
+    $(".what-showing4").click(function(){
+        $(".what-showing4").hide();
+        $(".PM").show();
+    });
 });
