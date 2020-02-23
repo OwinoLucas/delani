b19d98da809fea784e57aed3d64477f11949a647
Lucas Owino <lucowish35@gmail.com>
2020-02-23 13:25:08 +0300

    Change the fadeOut/fadeIn() method to fadeTo() method

--- js/script.js
+++ js/script.js
@@ -37,64 +37,64 @@ $(document).ready(function(){
         $("#work4").fadeTo("fast", 1);
     });
     $("#work3").hover(function(){
-        $("#work3").fadeOut();
-        $("#work03").fadeIn();
+        $("#work3").fadeTo("fast", 0.4);
+        $("#work03").show();
     });
     $("#work03").hover(function(){
-        $("#work03").fadeOut();
-        $("#work3").fadeIn();
+        $("#work03").hide();
+        $("#work3").fadeTo("fast", 1);
     });
     $("#work2").hover(function(){
-        $("#work2").fadeOut();
-        $("#work02").fadeIn();
+        $("#work2").fadeTo("fast", 0.4);
+        $("#work02").show();
     });
     $("#work02").hover(function(){
-        $("#work02").fadeOut();
-        $("#work2").fadeIn();
+        $("#work02").hide();
+        $("#work2").fadeTo("fast", 1);
     });
     $("#work1").hover(function(){
-        $("#work1").fadeOut();
-        $("#work01").fadeIn();
+        $("#work1").fadeTo("fast", 0.4);
+        $("#work01").show();
     });
     $("#work01").hover(function(){
-        $("#work01").fadeOut();
-        $("#work1").fadeIn();
+        $("#work01").hide();
+        $("#work1").fadeTo("fast", 1);
     });
     
 });
 
 $(document).ready(function(){
     $("#work5").hover(function(){
-        $("#work5").fadeOut();
-        $("#work05").fadeIn();
+        $("#work5").fadeTo("fast", 0.4);
+        $("#work05").show();
     });
     $("#work05").hover(function(){
-        $("#work05").fadeOut();
-        $("#work5").fadeIn();
+        $("#work05").hide();
+        $("#work5").fadeTo("fast", 1);
     });
     $("#work6").hover(function(){
-        $("#work6").fadeOut();
-        $("#work06").fadeIn();
+        $("#work6").fadeTo("fast", 0.4);
+        $("#work06").show();
     });
     $("#work06").hover(function(){
-        $("#work06").fadeOut();
-        $("#work6").fadeIn();
+        $("#work06").hide();
+        $("#work6").fadeTo("fast", 1);
     });
     $("#work7").hover(function(){
-        $("#work7").fadeOut();
-        $("#work07").fadeIn();
+        $("#work7").fadeTo("fast", 0.4);
+        $("#work07").show();
     });
     $("#work07").hover(function(){
-        $("#work07").fadeOut();
-        $("#work7").fadeIn();
+        $("#work07").hide();
+        $("#work7").fadeTo("fast", 1);
     });
     $("#work8").hover(function(){
-        $("#work8").fadeOut();
-        $("#work08").fadeIn();
+        $("#work8").fadeTo("fast", 0.4);
+        $("#work08").show();
     });
     $("#work08").hover(function(){
-        $("#work08").fadeOut();
-        $("#work8").fadeIn();
+        $("#work08").hide();
+        $("#work8").fadeTo("fast", 1);
     });
     
 });
